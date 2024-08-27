599793ae9ae665c76c64ed01960bafc6946e18a7
Luis Carrillo <lois@localhost.localdomain>
2024-08-26 23:28:52 -0400

    Model update

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = application = bottle.Bottle()
 
 MINIMAL_CORS = {
 	'Content-type' : 'application/json',
-	'Access-Control-Allow-Origin' : 'localhost:8081',
+	'Access-Control-Allow-Origin' : 'localhost:8090',
 }
 
 DB_PATH = '/mnt/sdcard/NivelAcademico.db'
@@ -21,5 +21,30 @@ def asset_files(filepath):
 def error404(error):
 	return 'Nothing here, sorry'
 
+
+@app.route('/NIVEL_ACADEMICO/v1/', method='GET')
+def get_courses():
+	try:
+		with sqlite3.connect(DB_PATH) as connection:
+			with closing(connection.cursor()) as cursor:
+				cursor.execute("SELECT UPPER([id]) AS id, UPPER([descripción]) AS nm, (([estados] & 1) = 1) AS e FROM [Nivel Académico]", ())
+				cursor.row_factory = sqlite3.Row
+				ds_ = [dict(r) for r in cursor.fetchall()]
+				return bottle.HTTPResponse(body=json.dumps({'data': ds_}), status=200, headers=MINIMAL_CORS)
+	except sqlite3.OperationalError as e:
+		return bottle.HTTPResponse(body=json.dumps({'msg': str(e)}), status=500)
+
+@app.route('/NIVEL_ACADEMICO/v1/_/', method='GET')
+def get_courses():
+	try:
+		with sqlite3.connect(DB_PATH) as connection:
+			with closing(connection.cursor()) as cursor:
+				cursor.execute("SELECT UPPER([id]) AS id, UPPER([descripción]) AS nm FROM [Nivel Académico] WHERE [estados] >= 5", ())
+				cursor.row_factory = sqlite3.Row
+				ds_ = [dict(r) for r in cursor.fetchall()]
+				return bottle.HTTPResponse(body=json.dumps({'data': ds_}), status=200, headers=MINIMAL_CORS)
+	except sqlite3.OperationalError as e:
+		return bottle.HTTPResponse(body=json.dumps({'msg': str(e)}), status=500)
+
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=8081, reloader=True, debug=True)
+	app.run(host='0.0.0.0', port=8090, reloader=True, debug=True)
--- model/NivelAcademico_[Sqlite3][ES].sql
+++ model/NivelAcademico_[Sqlite3][ES].sql
@@ -1,5 +1,16 @@
+-- ---------------------------------------------------------------------
+-- @description		Tabla diccionario de los NIVELES ACADÉMICOS EDUCATIVOS 
+-- @author			Luis Carrillo Gutiérrez
+-- @date			2022/03/25
+-- @comment			Tabla diccionario para la comprensión de los niveles
+--					gestionados por una institución educativa, ie. inicial
+--					primaria, secundaria...
+-- 0008
+-- ---------------------------------------------------------------------
+
 CREATE TABLE IF NOT EXISTS [Nivel Académico] (
-	[id] CHAR(36) NOT NULL PRIMARY KEY,
+	[id] CHAR(2) NOT NULL PRIMARY KEY,
+	[fechaDeVigencia] DATE NOT NULL,
 	[descripción] TEXT NOT NULL,
 	[estados] INTEGER NOT NULL
 ) WITHOUT ROWID;
